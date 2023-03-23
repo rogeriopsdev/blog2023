f7bfe4ebd3583bf1bd8dfca45bec6ed6c6ad8de6
dev <rogerio.pereira@ifto.edu.br>
2023-03-22 22:28:28 -0300

    bd

--- blog/settings.py
+++ blog/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog2023',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blogapp.models import Post,Tema
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Tema)
